ca781851086cae3ad25b70fc93c45e01704f68a2
jiangxincode <jiangxinnju@163.com>
2014-03-09 10:46:40 +0800

    add template.sh

---
+++ template.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Program:
+#	Program creates three files, which named by user's input and date command.
+# Version:
+#	Version 0.01
+# Author:
+#	Create by jiangxin
+#	jiangxinnju@126.com
+# History:
+#	2014/01/01	jiangxin	First release
+# Warning:
+#	This procedure without any warranty.
+
+echo "hello"
