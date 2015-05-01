20632f4a7647d79a9fe758251938e067077b8d3b
jiangxincode <jiangxinnju@163.com>
2015-05-01 20:55:28 +0800

    update jdk _install_1.sh

--- jdk_install_1/install.sh
+++ jdk_install_1/install.sh
@@ -1,41 +1,60 @@
-echo "正在创建/usr/lib/jvm/目录"
-sudo mkdir /usr/lib/jvm/
+#!/bin/bash
+
+echo "创建/usr/lib/jvm/目录"
+if [ ! -d /usr/lib/jvm ];
+then
+	sudo mkdir /usr/lib/jvm
+fi
 echo "目录/usr/lib/jvm/创建成功"
 
-echo "正在解压缩jdk-7u51-linux-i586.tar.gz"
-tar -zxvf jdk-7u51-linux-i586.tar.gz
-echo "解压缩成功"
-
-echo "正在将jdk1.7.0_51/目录复制到/usr/lib/jvm/jdk1.7.0_51/"
-sudo cp -r -p jdk1.7.0_51/ /usr/lib/jvm/jdk1.7.0_51/
-echo "复制成功"
+echo "正在安装JDK和Eclipse"
+os_version=`uname -a`
+echo $os_version
+architecture="64"
+echo "$os_version" | grep -q "$architecture"
+if [ $? -eq 0 ]
+then
+	echo "您正在使用64位操作系统，为您选择64位JDK和eclipse"
+	sudo tar -zxvf jdk-7u60-linux-x64.tar.gz -C /usr/lib/jvm/
+	sudo chown -R jiangxin:jiangxin /usr/lib/jvm/jdk1.7.0_60
+	sudo tar -zxvf eclipse-java-luna-R-linux-gtk-x86_64.tar.gz -C /usr/bin/
+	sudo chown -R jiangxin:jiangxin /usr/bin/eclipse
+	sudo ln -s /usr/bin/eclipse/eclipse ~/Desktop/eclipse
+else
+	echo "您正在使用32位操作系统，为您选择32位JDK和eclipse"
+	sudo tar -zxvf jdk-7u60-linux-i586.tar.gz -C /usr/lib/jvm/
+	sudo chown -R jiangxin:jiangxin /usr/lib/jvm/jdk1.7.0_60
+	sudo tar -zxvf eclipse-java-luna-R-linux-gtk.tar.gz -C /usr/bin/
+	sudo chown -R jiangxin:jiangxin /usr/bin/eclipse
+	sudo ln -s /usr/bin/eclipse/eclipse ~/Desktop/eclipse
+fi
+echo "安装JDK和Eclipse成功"
 
 echo "配置环境变量"
-# touch environment  
-# echo "PATH=\"$PATH:/usr/lib/jvm/jdk1.7.0_51/bin\"" >> environment
-# echo "JAVA_HOME=/usr/lib/jvm/jdk1.7.0_51" >> environment
-# echo "CLASSPATH=.:%JAVA_HOME%/lib/dt.jar:%JAVA_HOME%/lib/tools.jar" >> environment
+
 # sudo mv /etc/environment /etc/environment.backup.java
-# sudo mv environment /etc
+# touch environment
+# sudo echo "PATH=\"$PATH:/usr/lib/jvm/jdk1.7.0_60/bin\"" >> environment
+# sudo echo "JAVA_HOME=/usr/lib/jvm/jdk1.7.0_60" >> environment
+# sudo echo "CLASSPATH=.:%JAVA_HOME%/lib/dt.jar:%JAVA_HOME%/lib/tools.jar" >> environment
+# sudo mv environment /etc/environment
 # source /etc/environment
 
 mv ~/.bashrc ~/.bashrc.backup.java
 cat ~/.bashrc.backup.java >> ~/.bashrc
-echo "PATH=\"$PATH:/usr/lib/jvm/jdk1.7.0_51/bin\"" >> ~/.bashrc
-echo "JAVA_HOME=/usr/lib/jvm/jdk1.7.0_51" >> ~/.bashrc
-echo "CLASSPATH=.:%JAVA_HOME%/lib/dt.jar:%JAVA_HOME%/lib/tools.jar" >> ~/.bashrc
+echo "PATH=\"$PATH:/usr/lib/jvm/jdk1.7.0_60/bin\"" >> ~/.bashrc
+echo "export JAVA_HOME=/usr/lib/jvm/jdk1.7.0_60" >> ~/.bashrc
+echo "export CLASSPATH=.:%JAVA_HOME%/lib/dt.jar:%JAVA_HOME%/lib/tools.jar" >> ~/.bashrc
 source ~/.bashrc
 echo "配置环境成功"
 
+# 如果有多个java版本需要进行以下配置（包括openjdk）
 echo "设置默认jdk"
-sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/java/jdk1.7.0_51/bin/java 300
-sudo update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/java/jdk1.7.0_51/bin/javac 300
+sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/jdk1.7.0_60/bin/java 300
+sudo update-alternatives --install /usr/bin/javac javac /usr/lib/jvm/jdk1.7.0_60/bin/javac 300
 sudo update-alternatives --config java
-echo "设置默认jdk成功"
+# echo "设置默认jdk成功"
 
-echo "清理临时文件"
-rm -rf jdk1.7.0_51/
-echo "清理完成"
 
 echo "测试是否安装成功"
 java -version
--- jdk_install_1/uninstall.sh
+++ jdk_install_1/uninstall.sh
@@ -1,11 +1,15 @@
-echo "正在删除/usr/lib/jvm/目录"
+echo "正在删除相关文件"
 sudo rm -rf /usr/lib/jvm/
+sudo rm -rf /usr/bin/eclipse/
+sudo rm -rf ~/Desktop/eclipse
 wait
-echo "删除/usr/lib/jvm/目录成功"
+echo "删除相关文件成功"
 
 echo "恢复配置文件"
-# sudo rm -f /etc/environment
+sudo rm /usr/bin/java /usr/bin/javac
+sudo rm /etc/alternatives/java /etc/alternatives/javac
+
 # sudo mv /etc/environment.backup.java /etc/environment
-rm  ~/.bashrc
 mv ~/.bashrc.backup.java ~/.bashrc
+source ~/.bashrc
 echo "恢复配置文件成功"
