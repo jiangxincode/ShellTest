14d6baa6e8a8f691e666b8886c3026bdd90506de
jiangxincode <jiangxinnju@163.com>
2016-03-18 19:31:19 +0800

    test

--- move.sh
+++ move.sh
@@ -3,13 +3,13 @@
 # 将当前目录下大于10K的文件转移到/tmp目录下
 # modified by jiangxin
 
-for FileName in `ls -l | awk '$5>10240 {print $9}'`
+for fileName in `ls -l | awk '$5>10240 {print $9}'`
 # shell script要求把command放在``中，awk的作用是分子段处理数据
 # $0代表整行，$1代表每一行第一个字段,$2代表每一行第二个字段，以此类推
 # $5代表文件或文件夹的大小，$9代表文件或文件夹的名称
 do
-sudo mv $FileName /tmp
-# 此处不要忘了sudo
+    sudo mv $fileName /tmp
 done
+
 ls -al /tmp
 echo "Done! "
